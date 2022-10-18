61ee55941b382a11aec6748f5788c0a18a467979
IsaacMbonye <mbonyeisac@gmail.com>
2022-10-18 13:10:29 +0000

    yes

--- main.h
+++ main.h
@@ -1,24 +1,24 @@
-#ifndef PRINT_F
+/**
+ *#ifndef PRINT_F
 #define PRINT_F
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
+
  * struct convert - defines a structure for symbols and functions
  *
  * @sym: The operator
  * @f: The function associated
- */
-struct convert
+ *struct convert
 {
 	char *sym;
 	int (*f)(va_list);
 };
 typedef struct convert conver_t;
 
-/*Main functions*/
+*Main functions*
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
 int _write_char(char);
@@ -29,13 +29,13 @@ int print_integer(va_list);
 int print_number(va_list);
 int print_binary(va_list);
 int print_reversed(va_list arg);
-int rot13(va_list);
+*int rot13(va_list);*
 int unsigned_integer(va_list);
 int print_octal(va_list list);
 int print_hex(va_list list);
 int print_heX(va_list list);
 
-/*Helper functions*/
+Helper functions
 unsigned int base_len(unsigned int, int);
 char *rev_string(char *);
 void write_base(char *str);
@@ -44,5 +44,49 @@ int print_unsgined_number(unsigned int);
 
 
 #endif
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/**
+ * * struct types_arguments - Struct op
+ * *
+ * * @t: The operator
+ * * @f: The function associated
+ * */
+typedef struct types_arguments
+{
+		char t;
+			int (*f)(va_list va);
+} types;
+int _putchar(char c);
+int _strlen(char *s);
+int print_number(va_list va);
+int binary(va_list va);
+int print_unsigned(va_list va);
+int hexa(va_list va);
+int hexa_upper(va_list va);
+int octal(va_list va);
+int print_rot(va_list va);
+char *malloc_str(char *str);
+char *rot13(char *p);
+int print_rev(va_list va);
+int hexa_special(unsigned int c);
+int stringhexa(va_list va);
+int pointer(va_list va);
+int hexa_pointer(unsigned long int c);
+int _printf(const char *format, ...);
+int t_char(va_list va);
+int t_string(va_list va);
+int print_number(va_list va);
 
 
+
+#endif
